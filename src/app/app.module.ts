a1aeeafde6888c8742b84d058f0eec28940c9cb5
Eduardo Chami <e.chami.tap@gmail.com>
2022-10-27 16:06:39 -0300

    generando shared y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     FormsModule
   ],
   providers: [],
